8d85fbfe5396923fd9efb9930407f77f76a67bc0
Dima Usatyuk <Shonen1988@gmail.com>
2020-03-26 15:47:31 +0200

    add volume seek bar

--- app/src/main/java/com/shonen/ukr/audiovideo/MainActivity.java
+++ app/src/main/java/com/shonen/ukr/audiovideo/MainActivity.java
@@ -2,20 +2,30 @@ package com.shonen.ukr.audiovideo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.MediaController;
+import android.widget.SeekBar;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    /**
+     * Parameters for UI components
+     */
     private VideoView myVideoView;
-    private Button playVideoBtn;
+    private Button playVideoBtn, btnPlayMusic, btnPauseMusic;
+    private SeekBar seekBarVolume;
+    /*
+     *  Parameters for multimedia components
+     */
+
     private MediaController mediaController;
-    private Button btnPlayMusuc, btnPauseMusic;
     private MediaPlayer mediaPlayer;
+    private AudioManager audioManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,16 +34,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         myVideoView = findViewById(R.id.MyVideoView);
         playVideoBtn = findViewById(R.id.btnPlayVideo);
-        btnPlayMusuc = findViewById(R.id.btnPlayMusic);
+        btnPlayMusic = findViewById(R.id.btnPlayMusic);
         btnPauseMusic = findViewById(R.id.btnPauseMusic);
+        seekBarVolume = findViewById(R.id.seekBarVolume);
+
 
         playVideoBtn.setOnClickListener(MainActivity.this);
-        btnPlayMusuc.setOnClickListener(MainActivity.this);
+        btnPlayMusic.setOnClickListener(MainActivity.this);
         btnPauseMusic.setOnClickListener(MainActivity.this);
 
         mediaController = new MediaController(MainActivity.this);
-        mediaPlayer = MediaPlayer.create(this,R.raw.audio_file);
+        mediaPlayer = MediaPlayer.create(this, R.raw.audio_file);
+
+        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        int maxVolumeOfUserDevice = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int currentVolumOFUserDevice = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+        seekBarVolume.setMax(maxVolumeOfUserDevice);
     }
+
 
     @Override
     public void onClick(View buttonView) {
